f6014f07d678051eedcdd50a7c04c63e7a559f1e
Dimserene <31881806+Dimserene@users.noreply.github.com>
2024-10-15 22:41:37 +0800

    Update AliceJokers.lua

--- Chunks/AliceJokers.lua
+++ Chunks/AliceJokers.lua
@@ -284,7 +284,7 @@ return {
 								-------------
 								--sendDebugMessage('delta')
 								local _card = v
-								local suit_prefix = string.sub(_card.base.suit, 1, 1)..'_'
+								local suit_prefix = SMODS.Suits[_card.base.suit].card_key..'_'
 								local rank_suffix = v.base.id - 1
 								if rank_suffix < 10 then rank_suffix = tostring(rank_suffix)
 								elseif rank_suffix == 10 then rank_suffix = 'T'
